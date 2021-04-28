e387b9dfe7e3a21f5fb0d0f3b1d111df7a8c7f1d
Y0NEKO <43428414+Y0NEKO@users.noreply.github.com>
2021-04-28 16:54:26 +0800

    Update 1.main.R
    add change thread function

--- scripts/1.main.R
+++ scripts/1.main.R
@@ -5,23 +5,23 @@ args=commandArgs(T)
 command = args[1]
 sheetfile = args[2]
 outpath = args[3]
+cln = as.numeric(args[4])
 
-
-PreData1 = function(filename,scarfull,scar){
+PreData1 = function(filename,scarfull,scar,cln){
   data0 = read.table(filename, stringsAsFactors=F, header=T)
   print("Find scar...")
-  data = FindScar(data0,scarfull,scar)
+  data = FindScar(data0,scarfull,scar,cln)
   print("Done")
 }
-PreDataBulk = function(filename,scarfull,scar){
+PreDataBulk = function(filename,scarfull,scar,cln){
   data0 = read.table(filename,stringsAsFactors=F)
   names(data0) = c("Read.Seq","UMI")
   data0$Cell.BC = data0$UMI
   print("Find scar...")
-  data = FindScar(data0,scarfull,scar)
+  data = FindScar(data0,scarfull,scar,cln)
   print("Done")
 }
-PreData2 = function(filename,scarfull1,scar1,scarfull2,scar2){
+PreData2 = function(filename,scarfull1,scar1,scarfull2,scar2,cln){
   data0 = read.table(filename, stringsAsFactors=F, header=T)
   if(!dir.exists("v1")){
     dir.create("v1")
@@ -30,15 +30,15 @@ PreData2 = function(filename,scarfull1,scar1,scarfull2,scar2){
     dir.create("v2")
   }
   print("Split and Find scar...")
-  data = SplitScar(data0,scarfull1,scar1,scarfull2,scar2)
+  data = SplitScar(data0,scarfull1,scar1,scarfull2,scar2,cln)
   print("Done")
 }
-CallScar = function(INDEL,Scar,segref){
+CallScar = function(INDEL,Scar,segref,cln){
   print("Scar to INDEL Range...")
-  data = INDELChangeForm(INDEL,Scar,segref,".")
+  data = INDELChangeForm(INDEL,Scar,segref,".",cln)
   print("Done")
   print("Select Consensus reads...")
-  INDELCons(INDEL,data,segref,".")
+  INDELCons(INDEL,data,segref,".",cln)
   print("Done")
 }
 
@@ -75,12 +75,12 @@ if(command == "PreData1"){
   sv = ReadFasta(files["Fasta"])
   scarfull = sv$scarfull
   scar = sv$scar
-  PreData1(files["CB_UMI"],scarfull,scar)
+  PreData1(files["CB_UMI"],scarfull,scar,cln)
 }else if(command == "PreDataBulk"){
   sv = ReadFasta(files["Fasta"])
   scarfull = sv$scarfull
   scar = sv$scar
-  PreDataBulk(files["CB_UMI"],scarfull,scar)
+  PreDataBulk(files["CB_UMI"],scarfull,scar,cln)
 }else if(command == "PreData2"){
   sv1 = ReadFasta(files["Fasta1"])
   scarfull1 = sv1$scarfull
@@ -88,12 +88,12 @@ if(command == "PreData1"){
   sv2 = ReadFasta(files["Fasta2"])
   scarfull2 = sv2$scarfull
   scar2 = sv2$scar
-  PreData2(files["CB_UMI"],scarfull1,scar1,scarfull2,scar2)
+  PreData2(files["CB_UMI"],scarfull1,scar1,scarfull2,scar2,cln)
 }else if(command == "CallScar"){
   segrf = ReadCutsite(files["CutSite"])
   INDEL = readRDS("indel.rds")
   Scar = read.table("UMI_reads_scar_full.txt",header = T)
-  CallScar(INDEL,Scar,segref)
+  CallScar(INDEL,Scar,segref,cln)
 }else if(command == "ScarAnalysis"){
   image = readRDS("image.rds")
   scarseg = ReadCutsite(files["CutSite"])
