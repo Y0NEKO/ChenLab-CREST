b2a97518251c34f38ca542e3ab9f316178fb6b56
Y0NEKO <43428414+Y0NEKO@users.noreply.github.com>
2021-04-28 16:56:46 +0800

    Update step_sc.sh
    add thread args

--- scripts/step_sc.sh
+++ scripts/step_sc.sh
@@ -4,19 +4,20 @@ conda activate r4-base
 input=$1
 sheet=$2
 outpath=$3
+thread=$4
 sh 0.preprocess_sc.sh ${input} ${outpath}
 
 #1.1 for two arrays 
 #find array and separate v1 / v2
-Rscript 1.main.R PreData2 ${sheet} ${outpath}
-Rscript 1.main.R CallScar ${sheet} ${outpath}/v1
-Rscript 1.main.R CallScar ${sheet} ${outpath}/v2
-Rscript 1.main.R ScarAnalysis ${sheet} ${outpath}/v1
-Rscript 1.main.R ScarAnalysis ${sheet} ${outpath}/v2
+Rscript 1.main.R PreData2 ${sheet} ${outpath} ${thread}
+Rscript 1.main.R CallScar ${sheet} ${outpath}/v1 ${thread}
+Rscript 1.main.R CallScar ${sheet} ${outpath}/v2 ${thread}
+Rscript 1.main.R ScarAnalysis ${sheet} ${outpath}/v1 ${thread}
+Rscript 1.main.R ScarAnalysis ${sheet} ${outpath}/v2 ${thread}
 
 #1.2 for one array
-Rscript 1.main.R PreData1 ${sheet} ${outpath}
-Rscript 1.main.R CallScar ${sheet} ${outpath}
-Rscript 1.main.R ScarAnalysis ${sheet} ${outpath}
+Rscript 1.main.R PreData1 ${sheet} ${outpath} ${thread}
+Rscript 1.main.R CallScar ${sheet} ${outpath} ${thread}
+Rscript 1.main.R ScarAnalysis ${sheet} ${outpath} ${thread}
 
 
