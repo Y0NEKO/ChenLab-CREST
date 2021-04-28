45069918eb376e45039062cf2d1f9be058f0e85e
Y0NEKO <43428414+Y0NEKO@users.noreply.github.com>
2021-04-28 16:56:04 +0800

    Update step_bulk.sh
    add thread args

--- scripts/step_bulk.sh
+++ scripts/step_bulk.sh
@@ -6,12 +6,13 @@ fq2=$2
 sheet1=$3
 sheet2=$4
 outpath=$5
+thread=$6
 #sh 0.preprocess_bulk_single.sh ${fq1} ${fq2} ${outpath}
-sh 0.preprocess_bulk.sh ${fq1} ${fq2} ${outpath}
-Rscript 1.main.R PreDataBulk ${sheet1} ${outpath}/v1
-Rscript 1.main.R CallScar ${sheet1} ${outpath}/v1
-Rscript 1.main.R ScarAnalysis ${sheet1} ${outpath}/v1
+sh 0.preprocess_bulk.sh ${fq1} ${fq2} ${outpath} ${thread}
+Rscript 1.main.R PreDataBulk ${sheet1} ${outpath}/v1 ${thread}
+Rscript 1.main.R CallScar ${sheet1} ${outpath}/v1 ${thread}
+Rscript 1.main.R ScarAnalysis ${sheet1} ${outpath}/v1 ${thread}
 
-Rscript 1.main.R PreDataBulk ${sheet2} ${outpath}/v2
-Rscript 1.main.R CallScar ${sheet2} ${outpath}/v2
-Rscript 1.main.R ScarAnalysis ${sheet2} ${outpath}/v2
+Rscript 1.main.R PreDataBulk ${sheet2} ${outpath}/v2 ${thread}
+Rscript 1.main.R CallScar ${sheet2} ${outpath}/v2 ${thread}
+Rscript 1.main.R ScarAnalysis ${sheet2} ${outpath}/v2 ${thread}
