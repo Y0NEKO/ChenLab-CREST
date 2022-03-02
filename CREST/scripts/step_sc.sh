1b051d1bb07fe7a84c3c9b5f216efcfbad236403
Y0NEKO <43428414+Y0NEKO@users.noreply.github.com>
2022-03-02 15:41:28 +0800

    Update step_sc.sh

--- CREST/scripts/step_sc.sh
+++ CREST/scripts/step_sc.sh
@@ -1,5 +1,3 @@
-source /home/csdn/miniconda3/bin/activate
-conda activate r4-base
 
 input=$1
 sheet=$2
@@ -8,14 +6,14 @@ sh 0.preprocess_sc.sh ${input} ${outpath}
 
 #1.1 for two arrays 
 #find array and separate v1 / v2
-Rscript 1.main.R PreData2 ${sheet} ${outpath} 16
-Rscript 1.main.R ScarAnalysis ${sheet} ${outpath}/v1
-Rscript 1.main.R ScarAnalysis ${sheet} ${outpath}/v2
-Rscript 1.main.R LineageAnalysis2 ${sheet} ${outpath}
+Rscript array_recovery_main.R PreData2 ${sheet} ${outpath} 16
+Rscript array_recovery_main.R ScarAnalysis ${sheet} ${outpath}/v1
+Rscript array_recovery_main.R ScarAnalysis ${sheet} ${outpath}/v2
+Rscript array_recovery_main.R LineageAnalysis2 ${sheet} ${outpath}
 
 #1.2 for one array
-Rscript 1.main.R PreData1 ${sheet} ${outpath} 16
-Rscript 1.main.R ScarAnalysis ${sheet} ${outpath}
-Rscript 1.main.R LineageAnalysis1 ${sheet} ${outpath}
+Rscript array_recovery_main.R PreData1 ${sheet} ${outpath} 16
+Rscript array_recovery_main.R ScarAnalysis ${sheet} ${outpath}
+Rscript array_recovery_main.R LineageAnalysis1 ${sheet} ${outpath}
 
 
