9dfdecb766b93dbbd1e140133a2758008c933c94
Y0NEKO <43428414+Y0NEKO@users.noreply.github.com>
2022-03-02 15:30:46 +0800

    Update and rename tree_method.R to lineage_tree_recovery_method.R

--- CREST/scripts/lineage_tree_recovery_method.R
+++ CREST/scripts/lineage_tree_recovery_method.R
@@ -1,5 +1,6 @@
-#try heatmap method 
-#S2&S3 compare
+#lineage tree recovery analysis functions
+#22.03.02 by lhx
+
 library(reshape2)
 library(dplyr)
 library(ggplot2)
@@ -24,183 +25,6 @@ library(circlize)
 library(igraph)
 library(ggraph)
 
-test_data = function()
-{
-  setwd("/picb/sysgenomics2/people/liuhengxin/P6_lineartree/sc_result/")
-  samplels = list.files()[c(9,11,12,15,16)]
-  #???ļ?--------
-  v1ls = lapply(paste0(samplels,"/v1/final_scarform.csv"), function(x) read.csv(x,header = T)) 
-  v2ls = lapply(paste0(samplels,"/v2/final_scarform.csv"), function(x) read.csv(x,header = T)) 
-  
-  celllist = list.files(path = samplels, "/*_celltype.csv",full.names = T)
-  celllist2 = celllist;celllist2[4:5] = c("S2_D51/S2_D51_celltype2.csv","S3_D51/S3_D51_celltype2.csv")
-  Cells = lapply(celllist, ReadCell)
-  names(Cells) = names(v1ls) = names(v2ls) = samplels
-  bl1 = read.table("blacklisttag_select_v1.tsv",header = T,sep = "\t")
-  bl2 = read.table("blacklisttag_select_v2.tsv",header = T,sep = "\t")
-  
-  rawcell = c("7-N_Da","0-Prog_FP","3-Prog_NKX2-2","4-Prog_NKX6-1","1-N_Sero","5-N_Glu","6-N_GABA","9-N_Motor",
-              "0-FP_SHH", "2-FP_proli","4-Prog_NKX2-9","8-VLMC",
-              "5-N_Gaba","2-N_Sero1","3-N_Sero2","1-N_Glu","7-N_Motor2","4-Prog_LFP","6-N_Motor1",
-              "1-Meten_Glu","4-DA","5-N_GABA")
-  concell = c("DA","Prog_FP","Prog_NKX2-2","Prog_NKX6-1","Sero","Glu","GABA","Motor","FP_SHH","FP_proli","Prog_NKX2-9","VLMC",
-              "GABA","Sero1","Sero2","Glu","Motor2","Motor1","Prog_LFP","Meten_Glu","DA","GABA")
-  library(stringi)
-  for (i in 1:5) {
-    Cells[[i]]$Cell.type = stri_replace_all_fixed(Cells[[i]]$Cell.type,rawcell,concell,vectorize_all = FALSE)
-  }
-  
-  
-  #?????߼??????Ľ???��??ģ
-  #build tree-----------------------------
-  dv13 = v1ls$S3_D51
-  dv12 = v1ls$S2_D51
-  dv23 = v2ls$S3_D51
-  dv22 = v2ls$S2_D51
-  cell3 = Cells$S3_D51
-  cell2 = Cells$S2_D51
-  dv13 = merge(dv13[,c("Cell.BC","umim")],cell3,by="Cell.BC");colnames(dv13)[2] = "pattern"
-  dv12 = merge(dv12[,c("Cell.BC","umim")],cell2,by="Cell.BC");colnames(dv12)[2] = "pattern"
-  dv23 = merge(dv23[,c("Cell.BC","umim")],cell3,by="Cell.BC");colnames(dv23)[2] = "pattern"
-  dv22 = merge(dv22[,c("Cell.BC","umim")],cell2,by="Cell.BC");colnames(dv22)[2] = "pattern"
-  
-  prefix = c("v1.","v2.")
-  data3 = list(dv13,dv23)
-  data2 = list(dv12,dv22)
-  bl = list(bl1,bl2)
-  outname3 = "/picb/sysgenomics2/people/liuhengxin/P6_lineartree/tree_count/tree_test_6_29/S3_test"
-  outname2 = "/picb/sysgenomics2/people/liuhengxin/P6_lineartree/tree_count/tree_test_6_29/S2_test"
-  # outname = "/picb/sysgenomics2/people/liuhengxin/P6_lineartree/tree_count/tree_test_6_29/S3_test"
-  s3tree = TreeConstruct(data3,bl,prefix,outname3,cell3)
-  s2tree = TreeConstruct(data2,bl,prefix,outname2,cell2)
-  s3tree = CirclePlot1(s3tree,outname3)
-  s2tree = CirclePlot1(s2tree,outname2)
-  s2psd = PseudoAna(s2tree,pseudo)
-  s3psd = PseudoAna(s3tree,pseudo)
-  s2psdst = data.frame("sample" = "S2",
-                       "FC" = s2psd$psdfc$pseado.to / s2psd$psdfc$pseado.from) 
-  s3psdst = data.frame("sample" = "S3",
-                       "FC" = s3psd$psdfc$pseado.to / s3psd$psdfc$pseado.from) 
-  psdst = rbind(s2psdst,s3psdst)
-  psdst = psdst[which(psdst$FC>1),]
-  ggplot(psdst,aes(x = sample,y = log2(FC))) + geom_boxplot()
-  
-  
-  s2tree = readRDS("../tree_count/s2tree.rds")
-  s3tree = readRDS("../tree_count/s3tree.rds")
-  
-  outname2 = "../tree_count/method_test/method1_s2"
-  outname3 = "../tree_count/method_test/method1_s3"
-  MyHeatmap(s2tree,outname2)
-  MyHeatmap(s3tree,outname3)
-  #分析s2 s3 编辑深度差异
-  {
-    
-    
-    
-    leveldfs2 = StatLevel(s2tree$edge,s2tree$vertice,"s2")
-    leveldfs3 = StatLevel(s3tree$edge,s3tree$vertice,"s3")
-    
-    leveldfr = rbind(leveldfr2,leveldfr3)
-    # leveldfs2$sample = "s2"
-    # leveldfs3$sample = "s3"
-    leveldft = rbind(leveldfs2,leveldfs3)
-    # leveldft$level = as.factor(as.character(leveldft$level))
-    # leveldft$sizet = log2(leveldft$sizet+1)
-    
-    # p2.1 = ggboxplot(leveldft,x = "level", y ="sizet", color = "sample") + 
-    #   stat_compare_means(aes(group = sample),label = "p.format") + ylab("cell numbers") +
-    #   theme_ipsum(base_family = "sans")
-    # p2.2 = ggboxplot(leveldft,x = "level", y ="celltypes", color = "sample") + 
-    #   stat_compare_means(aes(group = sample),label = "p.format") + ylab("cell type numbers") +
-    #   theme_ipsum(base_family = "sans")
-    # 
-    # 
-    # levelar = leveldft %>% group_by(sample,level) %>% summarise(count=n())
-    # p2.3 = ggbarplot(levelar,x = "level",y = "count",color="sample",position = position_dodge(0.8)) + 
-    #   ylab("array number") +
-    #   theme_ipsum(base_family = "sans")
-    # p2.3
-    # 
-    # ggexport(ggarrange(p2.1,p2.2,p2.3,nrow = 1,ncol = 1),
-    #          filename = "../tree_count/method_test/method7_s2&s3_level_edit_compare.pdf",height = 5,width = 7)
-    # 
-    length(unique(leveldfs2$arrays))
-    length(unique(leveldfs3$arrays))
-    
-    head(leveldfs2)
-    vertice2 = s2tree$vertice
-    vertice3 = s3tree$vertice
-    vertice2[which(vertice2$cluster == "NONE"),"size"] = 0
-    vertice3[which(vertice3$cluster == "NONE"),"size"] = 0
-    head(vertice2)
-    length(unique(s2tree$cm$tags))/sum(vertice2$size)
-    length(unique(s3tree$cm$tags))/sum(vertice3$size)
-    sts23 = data.frame("sample" = c("S2","S3"),
-                       "array" = c(length(unique(s2tree$cm$tags)),length(unique(s3tree$cm$tags))),
-                       "cell" = c(sum(vertice2$size),sum(vertice3$size)))
-    sts23$density = sts23$array/sts23$cell
-    sts23
-    
-    
-    # leveldfr = leveldfr[order(leveldfr$norm),]
-    # leveldfr$rank = 1:nrow(leveldfr)
-    
-    # ggplot(leveldfr,aes(x=norm, col=sample)) +
-    #   stat_ecdf()
-    
-    p3.1 = ggplot(leveldfr,aes(x=norm,y = ecdf, col=sample)) +
-      geom_point(aes(size = complexity),alpha = 0.5) +
-      scale_color_ipsum() +
-      theme_bw(base_family = "sans") +
-      ylab("ecdf of subcluster size") + xlab("normalize size of subcluster")
-    p3.1
-    
-    p3.2 = ggplot(leveldfr,aes(x=norm,y = complexity, col=sample)) +
-      geom_point(alpha = 0.5) + 
-      scale_color_ipsum() +
-      theme_bw(base_family = "sans") +
-      ylab("complexity of subcluster") + xlab("normalize size of subcluster")
-    
-    sts23 = melt(sts23[1:3])
-    p3.3 = ggplot(sts23,aes(x=variable, y = value, fill=sample)) +
-      geom_bar(stat = "identity",position = position_dodge(),width = 0.5) + 
-      scale_fill_ipsum() +
-      theme_bw(base_family = "sans") +
-      ylab("") + xlab("")
-    p3.3
-    
-    
-    ggexport(ggarrange(p3.1,p3.2,p3.3,nrow = 1,ncol = 1),
-             filename = "../tree_count/method_test/s2&s3_compare.pdf",height = 5,width = 7)
-    
-    
-    ggplot(leveldfr,aes(x=rank,y = norm, col=sample)) +
-      geom_point(aes(size = complexity),alpha = 0.5) +
-      ylab("normalize subcluster size") + xlab("subcluster rank by size")
-    
-    # ggplot(leveldfr,aes(x=rank,y = norm, col=sample)) +
-    #   geom_point(aes(size = complexity),alpha = 0.5) +
-    #   ylab("normalize subcluster size") + xlab("subcluster rank by size")
-    # scale_fill_distiller(palette = "RdPu")
-    
-    
-    
-    leveldfr.e = NULL
-    for (i in 1:nrow(leveldfr)) {
-      leveldfr.e = rbind(leveldfr.e,leveldfr[rep(i,leveldfr$sizet[i]),])
-    }
-    
-    ggplot(leveldfr.e,aes(x=norm, col=sample)) + geom_histogram(binwidth = 0.001)
-    
-    ggplot(leveldfr[which(leveldfr$norm>0.03),],aes(x=sample, y=complexity)) + geom_boxplot()
-    #计算 complexity
-    
-    write.csv(leveldft,file = "../final_result/s2_s3/s2_s3_compare_leveldf.csv",row.names = F,quote = F)
-    write.csv(leveldfr,file = "../final_result/s2_s3/s2_s3_compare_leveldf_root.csv",row.names = F,quote = F)
-    
-  }
-}
 
 ReadCell = function(cellfile){
   Cells<-read.csv(cellfile,header = T,stringsAsFactors = F)
@@ -234,8 +58,6 @@ CirclePlot1 = function(tree,outname){
   tree_cell_count = cm2 %>% group_by(tags, celltype) %>% dplyr::summarise(count = sum(n()))
   
   nodesize = tree_cell_count
-  # nodesize = s2tree$count
-  # nodesize$size = rowSums(s2tree$count[-1])
   colnames(nodesize) = c("tags","cluster","size")
   
   nodesize = nodesize[which(nodesize$tags %in% unique(c(edge$from,edge$to))),]
@@ -243,11 +65,6 @@ CirclePlot1 = function(tree,outname){
                                     edge$to[!(edge$to %in% nodesize$tags)])), 
                   "cluster" = "NONE","size" = 0)
   vertices = rbind(N0,nodesize)
-  # vertices$group = NA
-  # vertices[which(vertices$tags %in% edgegrp$group),"group"] = vertices[which(vertices$tags %in% edgegrp$group),"tags"]
-  # vertices[which(vertices$tags == "node.N0"),"size"] = sum(nodesize$size)
-  # vertices$shortname = substr(vertices$tags,1,10)
-  # Rebuild the graph object
   #filter edges
   
   mygraph <- graph_from_data_frame(edge, vertices=vertices)
@@ -368,9 +185,6 @@ BlacklistFilter2 = function(data,bl){
   return(newdf)
   
 }
-TreeDataInitial = function(data,bl,cell){
-  
-}
 
 TreeConstruct = function(data,prefix,outname,cell){
   TagStat = function(x) {
@@ -390,21 +204,12 @@ TreeConstruct = function(data,prefix,outname,cell){
   tag = NULL
   
   for (i in 1:length(data)) {
-    # if(length(bl)>0){
-    #   tagi = apply(data[[i]],1,TagStat,blt = bl[[i]]$tag)
-    # }else{
-    #   tagi = apply(data[[i]],1,TagStat,blt = "")
-    # }
+    data[[i]] = data[[i]][which(data[[i]]$pattern %in% names(table(data[[i]]$pattern))[table(data[[i]]$pattern) > 1]),]
     tagi = apply(data[[i]],1,TagStat)
     tagi = do.call("rbind",tagi)
     tagi = na.omit(tagi)
-    # if(length(bl)>0){
-    #   tagi = tagi[which(!tagi$Tag %in% bl[[i]]$tag),]
-    # }
-    #black list filter
     tagi$Tag = paste(prefix[i], tagi$Tag, sep = "")
     tag = rbind(tag,tagi)
-    
   }
   
   
@@ -486,10 +291,6 @@ TreeConstruct = function(data,prefix,outname,cell){
   names(nodes) = paste("N",as.character(1:ncol(nodes)),sep = "")
   nodes$pathString = do.call(paste, c("N0",nodes, sep="/"))
   nodes$pathString = gsub("/NA","",nodes$pathString)
-  
-  # nodes = nodes[!is.na(nodes$N2),] 删去只有一个层的节点（为了看单个节点中包含的细胞关系，不删了）
-  # bn = names(table(nodes$N1)[which(table(nodes$N1)==1)])
-  # nodes = nodes[!nodes$N1 %in% bn,]
   
   nodes = nodes[which(!(is.na(nodes$N2) & (nodes$N1 %in% as.character(Tag_2$tags_paste)))),]
   
@@ -714,23 +515,12 @@ TreeConstruct = function(data,prefix,outname,cell){
                  geom = geom_segment,
                  mapping = aes(x = start, xend = end, y =y, yend = y,color=type),
                  size = 0.3)
-    # p1 = facet_plot(p,panel = "cell composition",data = tree_cell_plot,
-    #                 geom = geom_barh,
-    #                 mapping = aes(x = value,fill = variable),
-    #                 stat="identity") %>%
-    #   facet_plot(panel = "indel pattern", data = indel,
-    #              geom = geom_segment,
-    #              mapping = aes(x = start, xend = end, y =y, yend = y,color=type),
-    #              size = 1)
+
     
     
     #change facet grid
     gt1 = ggplot_gtable(ggplot_build(p1))
-    # gt1$widths[7] = 0.5*gt1$widths[7] # in this case it was colmun 7 - reduce the width by a half
-    # gt1$widths[9] = 0.5*gt1$widths[9] # in this case it was colmun 7 - reduce the width by a half
-    # grid.draw(gt1) # plot with grid draw
     gt1nt = ggplot_gtable(ggplot_build(p1nt))
-    
     gt2 = ggplot_gtable(ggplot_build(p2))
     gt2$widths[7] = 0.1*gt2$widths[7] # in this case it was colmun 7 - reduce the width by a half
     grid.draw(gt2) # plot with grid draw
@@ -739,179 +529,19 @@ TreeConstruct = function(data,prefix,outname,cell){
     gt2nt$widths[7] = 0.1*gt2nt$widths[7] # in this case it was colmun 7 - reduce the width by a half
     grid.draw(gt2nt) # plot with grid draw
     
-    ggexport(ggarrange(gt1,gt1nt,gt2,gt2nt,nrow = 1,ncol = 1),filename = paste0(outname,"_norm.pdf"),height = 0.12*length(tl),width = 12,units = "cm",limitsize = F)
-    # ggsave(gt,filename = paste0(outname,"_norm2.pdf"),height = 0.12*length(tl),width = 15,units = "cm",limitsize = F)
-    
+    ggexport(ggarrange(gt1,gt1nt,gt2,gt2nt,nrow = 1,ncol = 1),filename = paste0(outname,"_norm.pdf"),height = 0.12*length(tl),width = 12,units = "cm",limitsize = F)    
     
   }
   return(list("tree" = tree,"cm" = cm,"cm2" = cm2,"count" = tree_cell_count,
               "norm" = tree_cell_norm,"td" = td_cell,"edge" = td_wc2))
 }
-PseudoBuild = function(){
-  #test for trajectories
-  library(Signac)
-  library(Seurat)
-  library(SeuratWrappers)
-  library(monocle3)
-  library(Matrix)
-  library(ggplot2)
-  library(patchwork)
-  myPaths <- .libPaths()  
-  new <- c('/picb/sysgenomics2/people/liuhengxin/software/anaconda3/envs/r4-base/lib/R/library/')
-  myPaths <- c(myPaths, new) 
-  .libPaths(myPaths) 
-  .libPaths()
-  # BiocManager::install(c('BiocGenerics', 'DelayedArray', 'DelayedMatrixStats',
-  #                        'limma', 'S4Vectors', 'SingleCellExperiment',
-  #                        'SummarizedExperiment', 'batchelor', 'Matrix.utils'))
-  # devtools::install_github('cole-trapnell-lab/leidenbase')
-  # devtools::install_github('cole-trapnell-lab/monocle3')
-  # remotes::install_github('satijalab/seurat-wrappers')
-  
-  
-  trans = readRDS("../raw_data/S2S3_seurat_1212.rds")
-  pdf("../tree_count/method_test/umap_s2&s3.pdf",width = 8,height = 6)
-  DimPlot(trans,label = T)
-  dev.off()
-  trans.cds <- as.cell_data_set(trans)
-  trans.cds <- cluster_cells(cds = trans.cds, reduction_method = "UMAP")
-  trans.cds <- learn_graph(trans.cds, use_partition = TRUE)
-  trans.cds = order_cells(trans.cds, reduction_method = "UMAP")
-  pdf("../tree_count/method_test/pseudo_s2&s3.pdf",width = 8,height = 6)
-  plot_cells(
-    cds = trans.cds,
-    color_cells_by = "pseudotime",
-    show_trajectory_graph = TRUE
-  )
-  dev.off()
-  trans <- AddMetaData(
-    object = trans,
-    metadata = trans.cds@principal_graph_aux@listData$UMAP$pseudotime,
-    col.name = "pseudotime"
-  )
-  pseudo = trans.cds@principal_graph_aux@listData$UMAP$pseudotime
-  trans = saveRDS(trans,"../tree_count/method_test/s2&s3_trans.rds")
-  
-  pseudo = as.data.frame(pseudo)
-  pseudo$BC = substr(rownames(pseudo),1,16)
-  pseudo = pseudo[which(pseudo$pseudo != Inf),]
-}
 
-#分析pseudo与深度的关系
-PseudoAna = function(tree,pseudo){
-  cmt = merge(tree$cm,pseudo,by.x = "Var1",by.y = "BC")
-  #计算深度和pseudo的关系
-  vertice = tree$vertice
-  edge = tree$edge
-  
-  StatLevel = function(edge){
-    arrays = unique(c(edge$from,edge$to))
-    arrays = arrays[which(arrays!="node.N0")]
-    leveldf = NULL
-    for (i in 1:length(arrays)) {
-      level = 0
-      # lt = arrays[1]
-      lt = arrays[i]
-      lti = lt
-      while (1) {
-        lti = edge[which(edge$to == lti),"from"]
-        level = 1 + level
-        if(lti == "node.N0"){break}
-      }
-      print(i)
-      leveldf = rbind(leveldf, data.frame("arrays" = lt, "level" = level))
-    }
-    return(leveldf)
-  }
-  
-  leveldf = StatLevel(tree$edge)
-  levp = merge(vertice,leveldf,by.x = "tags",by.y = "arrays")
-  levp = levp[which(levp$pseudotime>0),]
-  p3.1 = ggplot(levp,aes(x = as.character(level),y = pseudotime,color = cluster)) + geom_point() + theme_bw()
-  p3.1
-  # ggsave(p3.1,filename = "../tree_count/method_test/method6_pseudo_level_s3.pdf",width = 6,height = 4)
-  
-  #计算一个父子比值
-  tree$td
-  psdmean = cmt %>% group_by(tags,celltype) %>% summarise(pseado = mean(pseudo),count = sum(Freq))
-  td = tree$td
-  td = td[,which(!colnames(td)%in%c("celltype","count","norm"))]
-  tmp = merge(td,psdmean,by.y = "tags",by.x = "from",all=T,)
-  tmp = merge(tmp,psdmean,by.y = "tags",by.x = "to",all=T,suffixes = c(".from",".to"))
-  psdfc = tmp
-  psdfc = psdfc[which(!is.na(psdfc$pseado.from) & !is.na(psdfc$pseado.to)),]
-  psdfc$count = (psdfc$count.from + psdfc$count.to)/(abs(psdfc$count.to - psdfc$count.from)+1)
-  
-  p3.2 = ggplot(psdfc,aes(x = pseado.from, y = pseado.to)) + 
-    geom_point(aes(pch = celltype.from,color = celltype.to,size = count)) + 
-    geom_function(lty = 3,fun = function(x) x) +
-    theme_bw()
-  p3.2
-  # ggsave(p3.2,filename = "../tree_count/method_test/method6_pseudo_parent&child_s3.pdf",width = 8,height = 6)
-  return(list("levp" = levp,"psdfc" = psdfc,"figure.depth" = p3.1,"figure.parchi" = p3.2))
-  
-}
 
 #heatmap normalize,filter,spearman&edu
-MyHeatmapNv = function(tree,outname){
-  library(amap)
-  tree_cell_countm = tree$count;rownames(tree_cell_countm) = tree_cell_countm$tags ;tree_cell_countm = tree_cell_countm[-1]
-  # tree_cell_countm = tree_cell_countm[,names(which(colSums(tree_cell_countm)>30))]
-  tree_cell_countm = as.matrix(tree_cell_countm)
-  tree_cell_countm[which(tree_cell_countm>0)] = 1
-  
-  tree_cell_norm = tree_cell_countm[which(rowSums(tree_cell_countm>0)>1),]
-  tree_cell_norm = log(tree_cell_norm+1)
-  tree_cell_norm = t(apply(tree_cell_norm, 1, function(x) {x/sum(x)}))
-  
-  # add rectangles around groups highly supported by the data
-  #filter tree norm
-  
-  #euclidean distance
-  mx1 = as.matrix(dist(t(tree_cell_norm),method = "euclidean"))
-  mx1 = 1 - (mx1 - min(mx1))/max(mx1 - min(mx1))
-  pdf(paste0(outname,"_euclidean.pdf"),width = 10,height = 8)
-  p1 = Heatmap(as.matrix(mx1),heatmap_legend_param = list(title = "euclidean"),
-               clustering_distance_rows = "euclidean",
-               # clustering_distance_rows = function(x){x = dd1; return(x)},
-               clustering_distance_columns = "euclidean",
-               # clustering_distance_columns = function(x){x = dd1; return(x)},
-               clustering_method_columns = "complete",clustering_method_rows = "complete")
-  print(p1)
-  dev.off()
-  #spearman
-  mx2 = cor(tree_cell_norm,method = "spearman")
-  mx2[is.na(mx2)] = 0
-  pdf(paste0(outname,"_spearman.pdf"),width = 10,height = 8)
-  p2 = Heatmap(as.matrix(mx2),heatmap_legend_param = list(title = "spearman"),
-               clustering_distance_rows = "spearman",
-               # clustering_distance_rows = function(x){x = dd2; return(x)},
-               clustering_distance_columns = "spearman",
-               # clustering_distance_columns = function(x){x = dd2; return(x)},
-               clustering_method_columns = "complete",clustering_method_rows = "complete")
-  print(p2)
-  dev.off()
-  
-  dd1 = dist(mx1,method = "euclidean")
-  # dd1 = dist(t(tree_cell_norm),method = "euclidean")
-  hc = hclust(dd1, method = "complete")
-  dd2 = Dist(mx2, method = "spearman")
-  # dd2 = Dist(t(tree_cell_norm), method = "spearman")
-  hc2 = hclust(dd2, method = "complete")
-  
-  pdf(paste0(outname,"_pvcluster_tree.pdf"))
-  plot(hc, cex = 1,hang=-1,main = "euclidean") # display pv cluster dendogram
-  plot(hc2, cex = 1,hang=-1,main = "spearman") # display pv cluster dendogram
-  dev.off()
-  
-  tree$distfigure = list("euc" = p1,"spear" = p2,"euchc" = hc,"spearhc" = hc2)
-  return(tree)
-}
 MyHeatmap = function(tree,outname){
   library(amap)
   tree_cell_countm = tree$count;rownames(tree_cell_countm) = tree_cell_countm$tags;tree_cell_countm = tree_cell_countm[-1]
   tree_cell_countm = as.matrix(tree_cell_countm)
-  tree_cell_countm[which(tree_cell_countm>0)] = 1
   
   tree_cell_norm = tree_cell_countm[which(rowSums(tree_cell_countm>0)>1),]
   tree_cell_norm = log(tree_cell_norm+1)
@@ -944,6 +574,7 @@ MyHeatmap = function(tree,outname){
   tree$distfigure = list("abspearson" = p1,"abspearsonhc" = hc)
   return(tree)
 }
+
 #heatmap fisher p value
 CalEnrichScore = function(cellmx){
   cmt = melt(cellmx)
@@ -972,6 +603,7 @@ CalEnrichScore = function(cellmx){
   }
   return(cellft)
 }
+
 MyHeatmap2 = function(tree,outname){
   cellft = CalEnrichScore(tree$cm)
   rownames(cellft) = cellft$tags; cellft = cellft[-1]
